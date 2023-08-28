f7e2af54f8a6671d86b67c847d0830c1f4fc3ca4
Kevin Sequeira G <kevinsteven.07.sg@gmail.com>
2023-08-28 16:54:22 -0600

    alt shift f

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import '@/styles/globals.css'
 import { I18nextProvider } from 'react-i18next';
-import i18n from '../i18n.js'; 
 
 export default function App({ Component, pageProps }) {
-  return <I18nextProvider><Component {...pageProps} /></I18nextProvider>
+  return (
+    <I18nextProvider>
+      <Component {...pageProps} />
+    </I18nextProvider>
+  )
 }

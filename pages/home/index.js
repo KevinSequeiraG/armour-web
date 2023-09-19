7c8c5dc020ff862e8dfc9d2c342ccf902e6a1cb1
Kevin Sequeira G <kevinsteven.07.sg@gmail.com>
2023-09-19 17:27:22 -0600

    asdf

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,6 +1,9 @@
+import { useTranslation } from "react-i18next";
 import WebPageCard from "../../components/Cards/webPageCard";
 
 const Home = () => {
+    const { t } = useTranslation();
+
     return (
         <div className="">
             <div className="w-full px-4 my-7">
@@ -9,13 +12,13 @@ const Home = () => {
                         <input
                             type="text"
                             className="border-2 border-gray-300 bg-white w-[18rem] h-10 px-5 pr-10 rounded-full text-sm focus:outline-none"
-                            placeholder="Buscar página"
+                            placeholder={t("search.search-page")}
                         />
                         <button type="submit" className="absolute right-0 top-0 mt-3 mr-4">
                             <img src="./svgs/search.svg" className="w-[1rem] h-[1rem]" alt="rar"/>
                         </button>
                     </div>
-                    <button className="hover:bg-[#a39869] hover:text-gray-100 hover:border-gray-100 bg-[#EFE1A2] text-[#212429] px-4 py-2 rounded-[.5rem] border border-1 border-[#212429] font-semibold">Crear página</button>
+                    <button className="hover:bg-[#a39869] hover:text-gray-100 hover:border-gray-100 bg-[#EFE1A2] text-[#212429] px-4 py-2 rounded-[.5rem] border border-1 border-[#212429] font-semibold">{t("buttons.create-page")}</button>
                 </div>
             </div>
             <div className="grid grid-cols-5 mx-auto justify-items-center gap-4">

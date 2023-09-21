86076b06470959414275654f88e788e527dc7a1b
Erick <erickvrp25@gmail.com>
2023-09-20 20:55:12 -0600

    creo que ya

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,6 @@
+import { UserContext } from "@/context/UserContext";
 import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const Header = () => {
@@ -10,6 +11,7 @@ const Header = () => {
     const { t, i18n } = useTranslation();
     const [showConfig, setShowConfig] = useState(false);
     const configRef = useRef(null);
+    const { loggedUser } = useContext(UserContext);
 
     const changeLanguage = (lng) => {
         i18n.changeLanguage(lng);
@@ -48,19 +50,19 @@ const Header = () => {
         <>
             {isLoginPage || isBuilderPage ? null : <div className="bg-[#212429] h-[5rem] flex justify-end items-center">
                 <div className="flex flex-col text-right">
-                    <p className="mr-4 text-[#EFE1A2]">Kevin Sequeira</p>
+                    <p className="mr-4 text-[#EFE1A2] capitalize">{loggedUser?.name + " " + loggedUser?.lastName}</p>
                     <div className="relative mr-4" ref={configRef}>
                         <button onClick={() => setShowConfig(!showConfig)} className="mt-1 text-gray-400 bg-gray-200 rounded-full p-1"><img src="./svgs/config.svg"></img></button>
                         {showConfig && <div className="border border-1 border-[#F5F5F5] bg-gray-500 min-h-[5rem] min-w-[5rem] absolute right-3 px-3 py-4 rounded-xl text-left">
                             <p className="text-[#F5F5F5]">{t("config.language")}</p>
                             <div className='mt-2 flex items-center bg-[#F5F5F5] px-4 py-2 rounded-[1rem] border border-1 border-gray-900'>
-                                <span class="text-[1rem] font-medium text-gray-900 mr-3">EN</span>
-                                <label class="relative inline-flex items-center cursor-pointer" >
+                                <span className="text-[1rem] font-medium text-gray-900 mr-3">EN</span>
+                                <label className="relative inline-flex items-center cursor-pointer" >
                                     <div>
-                                        <input type="checkbox" value="" class="sr-only peer" checked={lngEsp} onClick={(e) => setLngEsp(!lngEsp)} />
-                                        <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-gray-700 dark:peer-focus:ring-gray-700 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-700"></div>
+                                        <input type="checkbox" value="" className="sr-only peer" checked={lngEsp} onClick={(e) => setLngEsp(!lngEsp)} />
+                                        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-gray-700 dark:peer-focus:ring-gray-700 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-700"></div>
                                     </div>
-                                    <span class="ml-3 text-[1rem] font-medium text-gray-900">ES</span>
+                                    <span className="ml-3 text-[1rem] font-medium text-gray-900">ES</span>
                                 </label>
                             </div>
                         </div>}

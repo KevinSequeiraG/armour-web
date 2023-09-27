99ba9157470f82f161104c225b88042055fdf664
Kevin Sequeira G <kevinsteven.07.sg@gmail.com>
2023-09-26 18:58:58 -0600

    Fix

--- pages/repSocialNetwork/index.js
+++ pages/repSocialNetwork/index.js
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 
 const SocialNetwork = () => {
     const { t } = useTranslation();
+    
     return (
         <div className="">
             <Head>

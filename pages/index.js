d664f9cf7f2a22866770e6d67b3a36244778a1d0
Kevin Sequeira G <kevinsteven.07.sg@gmail.com>
2023-09-26 19:19:00 -0600

    asdf

--- pages/index.js
+++ pages/index.js
@@ -28,15 +28,15 @@ export default function Home() {
   };
 
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}    >
-      ARMOUR WEB
+    <main className='h-screen w-screen loginBody'>
+      {/* ARMOUR WEB
       <h1>{t('welcome')}</h1>
       <p>{t('greeting', { name: 'Eri' })}</p>
       <div>
         <button className='mr-4 py-2 px-4 bg-gray-200 text-black font-bold rounded-xl hover:bg-gray-400' onClick={() => changeLanguage('en')}>English</button>
         <button className='mr-4 py-2 px-4 bg-gray-200 text-black font-bold rounded-xl hover:bg-gray-400' onClick={() => changeLanguage('es')}>Español</button>
         <button onClick={handleCreate}>Prueba de Firebase</button>
-      </div>
+      </div> */}
     </main>
   )
 }

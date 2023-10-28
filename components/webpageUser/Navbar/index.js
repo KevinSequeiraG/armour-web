aac60466f11820319f06c5af3c78e1dff8756aab
Kevin Sequeira G <kevinsteven.07.sg@gmail.com>
2023-10-27 21:34:55 -0600

    asdf

--- components/webpageUser/Navbar/index.js
+++ components/webpageUser/Navbar/index.js
@@ -43,7 +43,7 @@ const Navbar = ({ children, webpageData, setCurrentPage }) => {
                 <div className={`flex items-center w-full ${(webpageData?.navbar?.position === "top") ? "flex-row" : ""} ${(webpageData?.navbar?.position === "top" && webpageData?.navbar?.contentPosition === "t-center") ? "justify-center" : ""} ${(webpageData?.navbar?.position === "top" && webpageData?.navbar?.contentPosition === "t-left") ? "justify-start mx-3" : ""} ${(webpageData?.navbar?.position === "top" && webpageData?.navbar?.contentPosition === "t-right") ? "justify-end mr-3" : ""} ${(webpageData?.navbar?.position !== "top" && webpageData?.navbar?.contentPosition === "center") ? "justify-center" : ""} ${(webpageData?.navbar?.position !== "top" && webpageData?.navbar?.contentPosition === "top") ? "justify-start" : ""} ${(webpageData?.navbar?.position !== "top" && webpageData?.navbar?.contentPosition === "bottom") ? "justify-end" : ""}  ${(webpageData?.navbar?.position === "left") ? "flex-col" : ""} ${(webpageData?.navbar?.position === "right") ? "flex-col" : ""}`}>
                     {(webpageData?.logo && webpageData?.navbar?.contentPosition !== "bottom" && webpageData?.navbar?.contentPosition !== "t-right") && <img className={`max-w-[3rem] max-h-[3rem] ${(webpageData?.navbar?.position !== "top" && webpageData?.navbar?.contentPosition === "top") ? "mt-3" : (webpageData?.navbar?.position !== "top" && webpageData?.navbar?.contentPosition === "bottom") ? "mb-3" : "x"} ${webpageData?.navbar?.position === "top" ? "my-3" : "mx-auto"}`} src={webpageData?.logo} alt="logo" />}
                     <button className="mdx600:hidden block ml-2" onClick={() => { setMenuVisible(!menuVisible) }}><NavbarIcon color={webpageData?.navbar?.color} /></button>
-                    <div className="hidden mdx600:block">
+                    <div className={`hidden ${webpageData?.navbar?.position === "top" ? "mdx600:block" : "mdx600:flex flex-col"} `}>
                         {navbarOptions.map(option => {
                             return (
                                 <button onClick={() => { setCurrentPage(option.id) }} className={`px-4 py-2 border-y-1 font-semibold`}>{option.name}</button>
@@ -55,7 +55,7 @@ const Navbar = ({ children, webpageData, setCurrentPage }) => {
             </div>
             {
                 menuVisible && (
-                    <div style={mobileMenuStyles} className={`flex flex-col w-auto ${webpageData?.navbar?.position === "top" ? "w-full":"h-full"} `}>
+                    <div style={mobileMenuStyles} className={`flex flex-col w-auto ${webpageData?.navbar?.position === "top" ? "w-full" : "h-full"} `}>
                         {/* Aquí coloca las opciones del menú que deseas mostrar */}
                         {/* <button style={mobileOptionInMenuStyle} className={`px-4 py-2 border-y-1 font-semibold`}>Home</button>
                         <button style={mobileOptionInMenuStyle} className={`px-4 py-2 border-y-1 font-semibold`}>Ubicacion</button> */}
